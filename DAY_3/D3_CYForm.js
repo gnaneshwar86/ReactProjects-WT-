1b5bafe01301bb1250b040a410e32f13a603f68f
Gnaneshwar R L <gnaneshwarnani8605@gmail.com>
2024-08-30 16:16:38 +0530

    Day 3 Updated

---
+++ DAY_3/D3_CYForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Display from './Display';
+
+function App() {
+    const [city, setCity] = useState('');
+    const [submittedCity, setSubmittedCity] = useState('New York');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setSubmittedCity(city);
+    };
+
+    return (
+        <div style={{backgroundColor:'lightblue',
+          height:'50vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'}}>
+            <form onSubmit={handleSubmit}>
+            <h1>Weather Application</h1>
+                <input
+                    type="text"
+                    placeholder="Enter City Name"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                />
+                <button type="submit">Update Location</button>
+                <Display name={submittedCity} />
+            </form>
+        </div>
+    );
+}
+
+export default App;
